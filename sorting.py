c7b6d621b3ed2a0f2b494b0935ccdfb32b4e4fd6
Karolina Klimentova <257007@vutbr.cz>
2024-04-15 12:32:01 +0200

    Add bubble_sort function

--- sorting.py
+++ sorting.py
@@ -51,15 +51,36 @@ def selection_sort(list_of_numbers, direction="descending"):
     return list_of_numbers
 
 
+def bubble_sort(list_of_whole_numbers):
+    """
+    Sorts a list of numbers in ascending order using Bubble Sort.
+    :param list_of_whole_numbers: list of integers
+    :return: sorted list of integers
+    """
+    n = len(list_of_whole_numbers)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if list_of_whole_numbers[j] > list_of_whole_numbers[j+1]:
+                list_of_whole_numbers[j], list_of_whole_numbers[j+1] = list_of_whole_numbers[j+1], list_of_whole_numbers[j]
+
+    return list_of_whole_numbers
+
+
 def main():
     file_name = "numbers.csv"
     data = read_data(file_name)
     print("Original data:", data)
+
     for key in data:
         unsorted_series = data[key]
         sorted_series = selection_sort(unsorted_series)
         print("\nSorted series '{}':".format(key))
         print(sorted_series)
+    for key in data:
+        unsorted_series = data[key]
+        sorted_numbers = bubble_sort(unsorted_series)
+        print("\nSeřazená posloupnost '{}':".format(key))
+        print(sorted_numbers)
 
 
 if __name__ == '__main__':
